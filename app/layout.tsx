3881724a811d6a8607a73f6c29952b4fcaa50a82
Ihor Kliushnyk <igorator746@gmail.com>
2025-10-30 09:51:56 +0200

    Provided analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Manrope } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { MobileOverlay } from "@/shared/components/widgets/MobileOverlay/MobileOverlay";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${fontHeading.variable} ${fontText.variable}`}>
+        <Analytics />
         <SpeedInsights />
         <MobileOverlay />
         {children}
