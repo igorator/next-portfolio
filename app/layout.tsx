590fa3fc6cb25f39ee27a9846da27c55dd614c0a
Ihor Kliushnyk <igorator746@email.com>
2026-02-13 06:00:48 +0200

    Transparent ios bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
-    { media: "(prefers-color-scheme: dark)", color: "#000000" },
-  ],
+  themeColor: "transparent",
 };
 
 export const metadata: Metadata = {
